d7b3b2c3a46c8d45d87aa2773873a7af442c211c
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-04-23 22:59:52 +0530

    Create array.java

---
+++ array_java/array.java
@@ -0,0 +1,25 @@
+// ceating a fixted sized array 
+package array;
+
+public class array {
+    public static void main(String args[]){
+
+    
+    int [] marks= new int[7];// declartion of array 
+    marks[0]=65;// intializing the value
+    marks[1]=65;
+    marks[2]=64;
+    marks[3]=63;
+    marks[4]=56;
+    marks[5]=87;
+    marks[6]=97;
+
+    for(int i=0;i<marks.length;i++){
+        System.out.print(marks[i]+" ");
+    }
+    }
+}
+
+
+// ceating a array and using primitive method to insert the element 
+// we can also create using dynamic method 
