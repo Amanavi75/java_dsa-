4ec5d4ce1bcec87c7a6feda072649aed06503284
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-04-23 23:05:43 +0530

    Create reverse.java

---
+++ array_java/reverse.java
@@ -0,0 +1,18 @@
+package array;
+import java.util.*;
+public class reverse {
+    public static void main(String args[]) 
+    { 
+       Scanner scn = new Scanner(System.in);
+       int n = scn.nextInt();
+       int [] arr= new int[n];
+       for(int i=0;i<arr.length;i++){
+        arr[i]=scn.nextInt();
+       }
+       for(int i=n-1;i>=0;i--){
+        
+        
+
+         } scn.close();
+    }
+}
