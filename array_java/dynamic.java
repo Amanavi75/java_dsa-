9713c1efec8e1f0af470f08f1a137b994b48e07e
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-04-23 23:00:29 +0530

    Create dynamic.java

---
+++ array_java/dynamic.java
@@ -0,0 +1,22 @@
+package array;
+
+import java.util.ArrayList;
+
+public class dynamic {
+    public static void main(String args[]){
+      ArrayList<Integer> ab=new ArrayList<Integer>();
+      ab.add(10);
+      ab.add(20);
+      ab.add(30);
+      ab.add(40);
+      ab.add(40);
+      ab.add(50);
+
+      System.out.println(ab);
+
+    }
+}
+
+// here we have created an array of dynamic size which is volatile for us to operate different operation on the particular array 
+
+// add - add method is used insert the element in the th dyanmic array 
