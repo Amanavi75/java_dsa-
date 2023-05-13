83b29e132ffd00b2b07c5b5f5cb5bf90ccab50ad
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-05-13 12:22:22 +0530

    Create constructor.java

---
+++ oops/constructor.java
@@ -0,0 +1,29 @@
+package oops;
+
+public class constructor {
+     public static class person {
+        int age ;
+        String name ;
+        void sayhii() {
+            System.out.println(name + " "+age + " says hii");
+
+        }
+        person(){
+            // this is default constructor made by java 
+        }
+        person(int age , String name ){ // parameterized constructor ; same name as function  it is called when we fire it with a constructor 
+            this.age =age ;
+            this.name= name ;
+        }
+     }
+     public static void main(String[] args){
+        person p1 =new person();
+        p1.age =14;
+        p1.name ="vivek";
+        p1.sayhii();
+
+        person p2 =new person(16,"aman"); // calling parametrized constructor 
+        p2.sayhii();
+
+     }
+}
