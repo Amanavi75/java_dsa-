8931460a160a9f1fc083bc24d6ae76e32b5df0da
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-06-16 10:03:26 +0530

    Create heapsort.java

---
+++ heap/heapsort.java
@@ -0,0 +1,48 @@
+package heap;
+
+public class heapsort {
+    public static void heapify(int arr[],int i , int size ){
+        int left = 2*i+1;
+        int right = 2*i+2;
+        int maxIdx = i;
+
+        if(left<size && arr[left]>arr[maxIdx]){
+            maxIdx=left;
+        }
+        if(right<size && arr[right]>arr[maxIdx]){
+            maxIdx=right;
+        }if(maxIdx!=i){
+            int temp = arr[i];
+            arr[i]= arr[maxIdx];
+            arr[maxIdx]=temp;
+
+            heapify(arr,maxIdx,size);
+        }
+    }
+
+    public static void heapSort(int arr[]){
+        // step 1 = build maxheap
+        int n = arr.length;
+        for (int i=n/2;i>=0;i--){
+            heapify(arr,i,n);
+        }
+         // step 2 push largest element at the end   
+        // swap (largest with the last element )
+        for(int i=n-1;i>0;i--){
+            int temp = arr[0];
+            arr[0]=arr[i];
+            arr[i]=temp;
+
+            heapify(arr,0,i);
+        }
+       
+    }
+
+    public static void main(String args[]){
+        int arr[]={1,2,4,5,3};
+        heapSort(arr);
+        for(int i=0;i<arr.length;i++){ System.out.print(arr[i]+" ");
+        }
+    }
+}
+//firt of all we will create the maxheap after that we will run the loop  till arrys length /2 and run heapify after that we will push largest element at last and after that we will seperate that  element from the array agaain we will run heapify for the remaining element and the process will be repeated again
