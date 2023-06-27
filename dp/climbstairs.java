e265f7ec90eb64629cfb00dabfb91d4abc5a14cd
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-06-27 10:27:02 +0530

    Update climbstairs.java

--- dp/climbstairs.java
+++ dp/climbstairs.java
@@ -1,5 +1,5 @@
 package dp;
-
+import java.util.Arrays;
 public class climbstairs {
     public static int countWays(int n,int ways[]){
         if(n==0){
@@ -17,11 +17,11 @@ public class climbstairs {
          // this code is pretty much similar to fibonacci logic  
     }
     // we wiill use the memoization logic to  solve that particular topic 
-    public static void main (String args){
+    public static void main (String args[]){
         int n = 5;
         int ways[]=new int[n+1];
+        Arrays.fill(ways,-1);
         System.out.println(countWays(n, ways));
     }
 }
- 
  
